379673778c0e939c30752f016de553ba394f237a
Abraham Juliot <abrahamjuliot@gmail.com>
2020-05-27 00:44:49 +0000

    server

---
+++ server.js
@@ -0,0 +1,6 @@
+var connect = require('connect')
+var serveStatic = require('serve-static')
+
+connect()
+    .use(serveStatic(__dirname))
+    .listen(8080, () => console.log('Server running on 8080...'))
