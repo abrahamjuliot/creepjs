6e0339d33d13d9d27ea6775b15a5fa157a2c5130
Abraham <abrahamjuliot@gmail.com>
2020-08-19 15:17:47 +0000

    clientRects bug fix

--- creep.js
+++ creep.js
@@ -2015,7 +2015,7 @@
 				patch(divRendered, html`
 				<div id="rect-container">
 					<style>
-					.rects{width:10px;height:10px;max-width:100%}.absolute{position:absolute}#cRect1{border:solid 2.715px;border-color:#F72585;padding:3.98px;margin-left:12.12px}#cRect2{border:solid 2px;border-color:#7209B7;font-size:30px;margin-top:20px;transform:skewY(23.1753218deg)}#cRect3{border:solid 2.89px;border-color:#3A0CA3;font-size:45px;transform:scale(100000000000000000000009999999999999.99, 1.89);margin-top:50px}#cRect4{border:solid 2px;border-color:#4361EE;transform:matrix(1.11, 2.0001, -1.0001, 1.009, 150, 94.4);margin-top:11.1331px;margin-left:12.1212px;padding:4.4545px;left:239.4141px;top:8.5050px}#cRect5{border:solid 2px;border-color:#4CC9F0;margin-left:42.395pt}#cRect6{border:solid 2px;border-color:#F72585;transform:perspective(12890px) translateZ(101.5px);padding:12px}#cRect7{margin-top:-350.552px;margin-left:0.9099rem;border:solid 2px;border-color:#4361EE}#cRect8{margin-top:-150.552px;margin-left:15.9099rem;border:solid 2px;border-color:#3A0CA3}#cRect9{margin-top:-110.552px;margin-left:15.9099rem;border:solid 2px;border-color:#7209B7}#cRect10{margin-top:-315.552px;margin-left:15.9099rem;border:solid 2px;border-color:#F72585}
+					.rects{width:10px;height:10px;max-width:100%}.absolute{position:absolute}#cRect1{border:solid 2.715px;border-color:#F72585;padding:3.98px;margin-left:12.12px}#cRect2{border:solid 2px;border-color:#7209B7;font-size:30px;margin-top:20px;transform:skewY(23.1753218deg)}#cRect3{border:solid 2.89px;border-color:#3A0CA3;font-size:45px;transform:scale(100000000000000000000009999999999999.99, 1.89);margin-top:50px}#cRect4{border:solid 2px;border-color:#4361EE;transform:matrix(1.11, 2.0001, -1.0001, 1.009, 150, 94.4);margin-top:11.1331px;margin-left:12.1212px;padding:4.4545px;left:239.4141px;top:8.5050px}#cRect5{border:solid 2px;border-color:#4CC9F0;margin-left:42.395pt}#cRect6{border:solid 2px;border-color:#F72585;transform:perspective(12890px) translateZ(101.5px);padding:12px}#cRect7{margin-top:-350.552px;margin-left:0.9099rem;border:solid 2px;border-color:#4361EE}#cRect8{margin-top:-150.552px;margin-left:15.9099rem;border:solid 2px;border-color:#3A0CA3}#cRect9{margin-top:-110.552px;margin-left:15.9099rem;border:solid 2px;border-color:#7209B7}#cRect10{margin-top:-315.552px;margin-left:15.9099rem;border:solid 2px;border-color:#F72585}#cRect11{width:10px;height:10px;margin-left:15.0000009099rem;border:solid 2px;border-color:#F72585}#cRect12{width:10px;height:10px;margin-left:15.0000009099rem;border:solid 2px;border-color:#F72585}
 					</style>
 					<div id="cRect1" class="rects"></div>
 					<div id="cRect2" class="rects"></div>
@@ -2027,27 +2027,44 @@
 					<div id="cRect8" class="rects absolute"></div>
 					<div id="cRect9" class="rects absolute"></div>
 					<div id="cRect10" class="rects absolute"></div>
+					<div id="cRect11" class="rects"></div>
+					<div id="cRect12" class="rects"></div>
 				</div>
 				`)
+				
 
 				// get clientRects
 				const rectElems = doc.getElementsByClassName('rects')
 				const clientRects = [...rectElems].map(el => {
 					return toJSONParsed(el.getClientRects()[0])
 				})
-				if (!rectsLie) {
+				
+				let offsetLie = false
+				const { right: right1, left: left1 } = clientRects[10]
+				const { right: right2, left: left2 } = clientRects[11]
+				if (right1 != right2 || left1 != left2) {
+					console.log(right1, right2)
+					console.log(left1, left2)
+					offsetLie = { lies: [{ ['equal elements mismatch']: true }] }
+				}
+				
+				if (!rectsLie && !offsetLie) {
 					iframeRendered.parentNode.removeChild(iframeRendered)
 					const $hash = await hashify(clientRects)
 					return resolve({clientRects, $hash })
 				}
 				// document lie and send to trash
 				if (rectsLie) {
-					documentLie('clientRects', hashMini(clientRects), rectsLie)
+					documentLie('clientRectsAPILie', hashMini(clientRects), rectsLie)
+				}
+				if (offsetLie) {
+					documentLie('clientRectsOffsetLie', hashMini(clientRects), offsetLie)
 				}
 				// Fingerprint lie
 				iframeRendered.parentNode.removeChild(iframeRendered)
-				const $hash = await hashify(rectsLie)
-				return resolve({rectsLie, $hash })
+				const lies = { rectsLie, offsetLie }
+				const $hash = await hashify(lies)
+				return resolve({...lies, $hash })
 			}
 			catch (error) {
 				captureError(error)
