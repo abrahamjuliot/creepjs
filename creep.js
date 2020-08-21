e0f8808f4757ba264e5c1cc49fc59647724af54d
Abraham <abrahamjuliot@gmail.com>
2020-08-21 01:55:52 +0000

    convert worker to inline #58

--- creep.js
+++ creep.js
@@ -382,10 +382,82 @@
 	}
 
 	// worker
+	// https://stackoverflow.com/a/20693860
+	const newWorker = fn => {
+		const blobURL = URL.createObjectURL(new Blob(
+			['(', (''+fn), ')()'],
+			{ type: 'application/javascript' }
+		))
+		const worker = new Worker(blobURL)
+		URL.revokeObjectURL(blobURL)
+		return worker
+	}
+	// inline worker scope
+	const inlineWorker = async () => {
+		const caniuse = (fn, objChainList = [], args = [], method = false) => {
+			let api
+			try {
+				api = fn()
+			} catch (error) {
+				return undefined
+			}
+			let i, len = objChainList.length, chain = api
+			try {
+				for (i = 0; i < len; i++) {
+					const obj = objChainList[i]
+					chain = chain[obj]
+				}
+			}
+			catch (error) {
+				return undefined
+			}
+			return (
+				method && args.length ? chain.apply(api, args) :
+				method && !args.length ? chain.apply(api) :
+				chain
+			)
+		}
+
+		let canvas2d = undefined
+		try {
+			const canvas = new OffscreenCanvas(256, 256)
+			const context = canvas.getContext('2d')
+			const str = '%$%^LGFWE($HIF)'
+			context.font = '20px Arial'
+			context.fillText(str, 100, 100)
+			context.fillStyle = 'red'
+			context.fillRect(100, 30, 80, 50)
+			context.font = '32px Times New Roman'
+			context.fillStyle = 'blue'
+			context.fillText(str, 20, 70)
+			context.font = '20px Arial'
+			context.fillStyle = 'green'
+			context.fillText(str, 10, 50)
+			const getDataURI = async () => {
+				const blob = await canvas.convertToBlob()
+				const reader = new FileReader()
+				reader.readAsDataURL(blob)
+				return new Promise(resolve => {
+					reader.onloadend = () => resolve(reader.result)
+				})
+			}
+			canvas2d = await getDataURI() 
+		}
+		catch (error) { }
+		
+		const hardwareConcurrency = caniuse(() => navigator, ['hardwareConcurrency'])
+		const language = caniuse(() => navigator, ['language'])
+		const platform = caniuse(() => navigator, ['platform'])
+		const userAgent = caniuse(() => navigator, ['userAgent'])
+
+		postMessage({ hardwareConcurrency, language, platform, userAgent, canvas2d })
+		close()
+	}
+
 	const getWorkerScope = instanceId => {
 		return new Promise(resolve => {
 			try {
-				const worker = new Worker('worker.js')
+				const worker = newWorker(inlineWorker)
 				worker.addEventListener('message', async event => {
 					const { data, data: { canvas2d } } = event
 					data.system = getOS(data.userAgent)
