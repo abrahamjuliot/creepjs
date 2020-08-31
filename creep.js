9d7f9373b3365d1dd515c4891f89b4c28c5b5f39
Abraham <abrahamjuliot@gmail.com>
2020-08-31 03:56:05 +0000

    tidy math fingerprints #36

--- creep.js
+++ creep.js
@@ -382,9 +382,11 @@
 	// id known hash
 	const known = hash => {
 		const id = {
-			'a9577e66b31a58e41b41a56e134347683d9bb87757d235e5b5ba587e7f1e5855': 'Chromium', // math
+			'89455ebb9765644fb98068ec68fbad7fcaaf2768b2cb6e1bd062eee5790c00e8': 'Chromium', // math
 			'8356feb9253a56ba698e1868905974db482202405b0eec201411bdbfa2abbad0': 'Firefox', // math
-			'd1759815f3bf9947a69c5c90ff5ecaef0a8c2d8c815012db721363df29b59236': 'Safari', // math
+			'c1141e10c4d38a4ca1a49d9c7335fdfdcd7625b4ba04053a2f335434ec7e4d36': 'Safari', // math
+			'09525011e48d69f97b4486a09a7d84dcb702ecb091f28d27b15fdf422960b874': 'Tor Browser (Windows)', // math
+			'ddc8837ab98695120dae774f04dcf295d2414ffc03431360d46b70380224547a': 'Firefox (MacOS)', // math
 			'7757f7416b78fb8ac1f079b3e0677c0fe179826a63727d809e7d69795e915cd5': 'Chromium', // errors
 			'21f2f6f397db5fa611029154c35cd96eb9a96c4f1c993d4c3a25da765f2dd13b': 'Firefox', // errors
 			'd420d594c5a7f7f9a93802eebc3bec3fba0ea2dde91843f6c4746121ef5da140': 'Safari' // errors
@@ -2000,23 +2002,23 @@
 					
 					['cos', [n], `cos(${n})`, 0.9924450321351935, NaN, NaN, NaN],
 					['cos', [Math.PI], 'cos(Math.PI)', -1, NaN, NaN, NaN],
-					['cos', [bigN], `cos(${bigN})`, -0.10868049424995659, NaN, NaN, NaN],
-					['cos', [-1e308], 'cos(-1e308)', NaN, NaN, NaN, NaN],
-					['cos', [13*Math.E], 'cos(13*Math.E)', NaN, NaN, NaN, NaN],
-					['cos', [57*Math.E], 'cos(57*Math.E)', NaN, NaN, NaN, NaN],
-					['cos', [21*Math.LN2], 'cos(21*Math.LN2)', NaN, NaN, NaN, NaN],
-					['cos', [51*Math.LN2], 'cos(51*Math.LN2)', NaN, NaN, NaN, NaN],
-					['cos', [21*Math.LOG2E], 'cos(21*Math.LOG2E)', NaN, NaN, NaN, NaN],
-					['cos', [25*Math.SQRT2], 'cos(25*Math.SQRT2)', NaN, NaN, NaN, NaN],
-					['cos', [50*Math.SQRT1_2], 'cos(50*Math.SQRT1_2)', NaN, NaN, NaN, NaN],
-					['cos', [21*Math.SQRT1_2], 'cos(21*Math.SQRT1_2)', NaN, NaN, NaN, NaN],
-					['cos', [17*Math.LOG10E], 'cos(17*Math.LOG10E)', NaN, NaN, NaN, NaN],
-					['cos', [2*Math.LOG10E], 'cos(2*Math.LOG10E)', NaN, NaN, NaN, NaN],
+					['cos', [bigN], `cos(${bigN})`, -0.10868049424995659, NaN, -0.9779661551196617, NaN],
+					['cos', [-1e308], 'cos(-1e308)', -0.8913089376870335, NaN, 0.99970162388838, NaN],
+					['cos', [13*Math.E], 'cos(13*Math.E)', -0.7108118501064331, -0.7108118501064332, NaN, NaN],
+					['cos', [57*Math.E], 'cos(57*Math.E)', -0.536911695749024, -0.5369116957490239, NaN, NaN],
+					['cos', [21*Math.LN2], 'cos(21*Math.LN2)', -0.4067775970251724, -0.40677759702517235, -0.6534063185820197, NaN],
+					['cos', [51*Math.LN2], 'cos(51*Math.LN2)', -0.7017203400855446, -0.7017203400855445, NaN, NaN],
+					['cos', [21*Math.LOG2E], 'cos(21*Math.LOG2E)', 0.4362848063618998, 0.43628480636189976, NaN, NaN],
+					['cos', [25*Math.SQRT2], 'cos(25*Math.SQRT2)', -0.6982689820462377, -0.6982689820462376, NaN, NaN],
+					['cos', [50*Math.SQRT1_2], 'cos(50*Math.SQRT1_2)', -0.6982689820462377, -0.6982689820462376, NaN, NaN],
+					['cos', [21*Math.SQRT1_2], 'cos(21*Math.SQRT1_2)', -0.6534063185820198, NaN, NaN, NaN],
+					['cos', [17*Math.LOG10E], 'cos(17*Math.LOG10E)', 0.4537557425982784, 0.45375574259827833, NaN, NaN],
+					['cos', [2*Math.LOG10E], 'cos(2*Math.LOG10E)', 0.6459044007438142, NaN, 0.6459044007438141, NaN],
 
 					['cosh', [1], 'cosh(1)', 1.5430806348152437, NaN, NaN, NaN],
 					['cosh', [Math.PI], 'cosh(Math.PI)', 11.591953275521519, NaN, NaN, NaN],
-					['cosh', [492*Math.LOG2E], 'cosh(492*Math.LOG2E)', NaN, NaN, NaN, NaN],
-					['cosh', [502*Math.SQRT2], 'cosh(502*Math.SQRT2)', NaN, NaN, NaN, NaN],
+					['cosh', [492*Math.LOG2E], 'cosh(492*Math.LOG2E)', 9.199870313877772e+307, 9.199870313877774e+307, NaN, NaN],
+					['cosh', [502*Math.SQRT2], 'cosh(502*Math.SQRT2)', 1.0469199669023138e+308, 1.046919966902314e+308, NaN, NaN],
 
 					['expm1', [1], 'expm1(1)', 1.718281828459045, NaN, NaN, 1.7182818284590453],
 					['expm1', [Math.PI], 'expm1(Math.PI)', 22.140692632779267, NaN, NaN, NaN],
@@ -2026,13 +2028,13 @@
 
 					['hypot', [1, 2, 3, 4, 5, 6], 'hypot(1, 2, 3, 4, 5, 6)', 9.539392014169456, NaN, NaN, NaN],
 					['hypot', [bigN, bigN], `hypot(${bigN}, ${bigN})`, 8.288489826731116e+38, 8.288489826731114e+38, NaN, NaN],
-					['hypot', [2*Math.E, -100], 'hypot(2*Math.E, -100)', NaN, NaN, NaN, NaN],
-					['hypot', [6*Math.PI, -100], 'hypot(6*Math.PI, -100)', NaN, NaN, NaN, NaN],
-					['hypot', [2*Math.LN2, -100], 'hypot(2*Math.LN2, -100)', NaN, NaN, NaN, NaN],
-					['hypot', [Math.LOG2E, -100], 'hypot(Math.LOG2E, -100)', NaN, NaN, NaN, NaN],
-					['hypot', [Math.SQRT2, -100], 'hypot(Math.SQRT2, -100)', NaN, NaN, NaN, NaN],
-					['hypot', [Math.SQRT1_2, -100], 'hypot(Math.SQRT1_2, -100)', NaN, NaN, NaN, NaN],
-					['hypot', [2*Math.LOG10E, -100], 'hypot(2*Math.LOG10E, -100)', NaN, NaN, NaN, NaN],
+					['hypot', [2*Math.E, -100], 'hypot(2*Math.E, -100)', 100.14767208675259, 100.14767208675258, NaN, NaN],
+					['hypot', [6*Math.PI, -100], 'hypot(6*Math.PI, -100)', 101.76102278593319, 101.7610227859332, NaN, NaN],
+					['hypot', [2*Math.LN2, -100], 'hypot(2*Math.LN2, -100)', 100.0096085986525, 100.00960859865252, NaN, NaN],
+					['hypot', [Math.LOG2E, -100], 'hypot(Math.LOG2E, -100)', 100.01040630344929, 100.01040630344927, NaN, NaN],
+					['hypot', [Math.SQRT2, -100], 'hypot(Math.SQRT2, -100)', 100.00999950004999, 100.00999950005, NaN, NaN],
+					['hypot', [Math.SQRT1_2, -100], 'hypot(Math.SQRT1_2, -100)', 100.0024999687508, 100.00249996875078, NaN, NaN],
+					['hypot', [2*Math.LOG10E, -100], 'hypot(2*Math.LOG10E, -100)', 100.00377216279416, 100.00377216279418, NaN, NaN],
 
 					['log', [n], `log(${n})`, -2.0955709236097197, NaN, NaN, NaN],
 					['log', [Math.PI], 'log(Math.PI)', 1.1447298858494002, NaN, NaN, NaN],
@@ -2043,19 +2045,19 @@
 					['log10', [n], `log10(${n})`, -0.9100948885606021, NaN, NaN, NaN],
 					['log10', [Math.PI], 'log10(Math.PI)', 0.4971498726941338, 0.49714987269413385, NaN, NaN],
 					['log10', [Math.E], 'log10(Math.E)', 0.4342944819032518, NaN, NaN, NaN],
-					['log10', [34*Math.E], 'log10(34*Math.E)',NaN, NaN, NaN, NaN],
+					['log10', [34*Math.E], 'log10(34*Math.E)', 1.9657733989455068, 1.965773398945507, NaN, NaN],
 					['log10', [Math.LN2], 'log10(Math.LN2)', -0.1591745389548616, NaN, NaN, NaN],
-					['log10', [11*Math.LN2], 'log10(11*Math.LN2)',NaN, NaN, NaN, NaN],
+					['log10', [11*Math.LN2], 'log10(11*Math.LN2)', 0.8822181462033634, 0.8822181462033635, NaN, NaN],
 					['log10', [Math.LOG2E], 'log10(Math.LOG2E)', 0.15917453895486158, NaN, NaN, NaN],
-					['log10', [43*Math.LOG2E], 'log10(43*Math.LOG2E)',NaN, NaN, NaN, NaN],
+					['log10', [43*Math.LOG2E], 'log10(43*Math.LOG2E)', 1.792642994534448, 1.7926429945344482, NaN, NaN],
 					['log10', [Math.LOG10E], 'log10(Math.LOG10E)', -0.36221568869946325, NaN, NaN, NaN],
-					['log10', [7*Math.LOG10E], 'log10(7*Math.LOG10E)',NaN, NaN, NaN, NaN],
+					['log10', [7*Math.LOG10E], 'log10(7*Math.LOG10E)', 0.4828823513147936, 0.48288235131479357, NaN, NaN],
 					['log10', [Math.SQRT1_2], 'log10(Math.SQRT1_2)', -0.15051499783199057, NaN, NaN, NaN],
-					['log10', [2*Math.SQRT1_2], 'log10(2*Math.SQRT1_2)',NaN, NaN, NaN, NaN],
+					['log10', [2*Math.SQRT1_2], 'log10(2*Math.SQRT1_2)', 0.1505149978319906, 0.15051499783199063, NaN, NaN],
 					['log10', [Math.SQRT2], 'log10(Math.SQRT2)', 0.1505149978319906, 0.15051499783199063, NaN, NaN],
 					
-					['sin', [bigN], `sin(${bigN})`, 0.994076732536068, NaN, NaN, NaN],
-					['sin', [Math.PI], 'sin(Math.PI)', 1.2246467991473532e-16, NaN, NaN, NaN],
+					['sin', [bigN], `sin(${bigN})`, 0.994076732536068, NaN, -0.20876350121720488, NaN],
+					['sin', [Math.PI], 'sin(Math.PI)', 1.2246467991473532e-16, NaN, 1.2246063538223773e-16, NaN],
 
 					['sin', [39*Math.E], 'sin(39*Math.E)', -0.7181630308570677, -0.7181630308570678, NaN, NaN],
 					['sin', [35*Math.LN2], 'sin(35*Math.LN2)', -0.7659964138980511, -0.765996413898051, NaN, NaN],
@@ -2069,11 +2071,11 @@
 					['sinh', [Math.E], 'sinh(Math.E)', 7.544137102816975, NaN, NaN, NaN],
 					['sinh', [Math.LN2], 'sinh(Math.LN2)', 0.75, NaN, NaN, NaN],
 					['sinh', [Math.LOG2E], 'sinh(Math.LOG2E)', 1.9978980091062795, NaN, NaN, NaN],
-					['sinh', [492*Math.LOG2E], 'sinh(492*Math.LOG2E)', NaN, NaN, NaN, NaN],
+					['sinh', [492*Math.LOG2E], 'sinh(492*Math.LOG2E)', 9.199870313877772e+307, 9.199870313877774e+307, NaN, NaN],
 					['sinh', [Math.LOG10E], 'sinh(Math.LOG10E)', 0.44807597941469024, NaN, NaN, NaN],
 					['sinh', [Math.SQRT1_2], 'sinh(Math.SQRT1_2)', 0.7675231451261164, NaN, NaN, NaN],
 					['sinh', [Math.SQRT2], 'sinh(Math.SQRT2)', 1.935066822174357, NaN, NaN, 1.9350668221743568],
-					['sinh', [502*Math.SQRT2], 'sinh(502*Math.SQRT2)', NaN, NaN, NaN, NaN],
+					['sinh', [502*Math.SQRT2], 'sinh(502*Math.SQRT2)', 1.0469199669023138e+308, 1.046919966902314e+308, NaN, NaN],
 
 					['sqrt', [n], `sqrt(${n})`, 0.3507135583350036, NaN, NaN, NaN],
 					['sqrt', [Math.PI], 'sqrt(Math.PI)', 1.7724538509055159, NaN, NaN, NaN],
@@ -2081,13 +2083,13 @@
 					['tan', [-1e308], 'tan(-1e308)', 0.5086861259107568, NaN, NaN, 0.5086861259107567],
 					['tan', [Math.PI], 'tan(Math.PI)', -1.2246467991473532e-16, NaN, NaN, NaN],
 
-					['tan', [6*Math.E], 'tan(6*Math.E)', NaN, NaN, NaN, NaN],
-					['tan', [6*Math.LN2], 'tan(6*Math.LN2)', NaN, NaN, NaN, NaN],
-					['tan', [10*Math.LOG2E], 'tan(10*Math.LOG2E)', NaN, NaN, NaN, NaN],
-					['tan', [17*Math.SQRT2], 'tan(17*Math.SQRT2)', NaN, NaN, NaN, NaN],
-					['tan', [34*Math.SQRT1_2], 'tan(34*Math.SQRT1_2)', NaN, NaN, NaN, NaN],
-					['tan', [10*Math.LOG10E], 'tan(10*Math.LOG10E)', NaN, NaN, NaN, NaN],
-
+					['tan', [6*Math.E], 'tan(6*Math.E)', 0.6866761546452431, 0.686676154645243, NaN, NaN],
+					['tan', [6*Math.LN2], 'tan(6*Math.LN2)', 1.6182817135715877, 1.618281713571588, NaN, 1.6182817135715875],
+					['tan', [10*Math.LOG2E], 'tan(10*Math.LOG2E)', -3.3537128705376014, -3.353712870537601, NaN, -3.353712870537602],
+					['tan', [17*Math.SQRT2], 'tan(17*Math.SQRT2)', -1.9222955461799982, -1.922295546179998, NaN, NaN],
+					['tan', [34*Math.SQRT1_2], 'tan(34*Math.SQRT1_2)', -1.9222955461799982, -1.922295546179998, NaN, NaN],
+					['tan', [10*Math.LOG10E], 'tan(10*Math.LOG10E)', 2.5824856130712432, 2.5824856130712437, NaN, NaN], 
+										
 					['tanh', [n], `tanh(${n})`, 0.12238344189440875, NaN, NaN, 0.12238344189440876],
 					['tanh', [Math.PI], 'tanh(Math.PI)', 0.99627207622075, NaN, NaN, NaN],
 
@@ -2102,7 +2104,7 @@
 					['pow', [Math.SQRT2, -100], 'pow(Math.SQRT2, -100)', 8.881784197001191e-16, 8.881784197001154e-16, NaN, NaN],
 					['pow', [Math.SQRT2, -100], 'pow(Math.SQRT2, -100)', 8.881784197001191e-16, 8.881784197001154e-16, NaN, NaN],
 					
-					['polyfill', [2e-3 ** -100], 'polyfill pow(2e-3, -100)', NaN, NaN, NaN, NaN]
+					['polyfill', [2e-3 ** -100], 'polyfill pow(2e-3, -100)', 7.888609052210102e+269, 7.888609052210126e+269, NaN, NaN]
 				]
 				const data = {}
 				fns.forEach(fn => {
@@ -2132,7 +2134,7 @@
 					<div>results: ${
 						modal(id, header+results.join('<br>'))
 					}
-					<div>engine: ${known($hash)}</div>
+					<div>implementation: ${known($hash)}</div>
 				</div>
 				`)
 				return
