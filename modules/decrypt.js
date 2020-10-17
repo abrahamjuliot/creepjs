a588348253006e5a8f140ce1ea98231ce129b178
Abraham <abrahamjuliot@gmail.com>
2020-10-17 22:59:47 +0000

    adjust decrypt title

--- modules/decrypt.js
+++ modules/decrypt.js
@@ -3,7 +3,7 @@ export const decrypt = ({ require: [ userAgentData, hashMini ] }) => {
 		const report = userAgentData.filter(report => report.id == hash)[0]
 		if (report && report.decoded) {
 			const { uaSystem, decoded } = report
-			return `${decoded}${!uaSystem.length || uaSystem.length > 1 ? '' : ` (${uaSystem[0]})`}`
+			return `${decoded}${!uaSystem.length || uaSystem.length > 1 ? '' : ` on ${uaSystem[0]}`}`
 		}
 		else {
 			return 'unknown'
