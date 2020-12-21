636a91d20a416ac65eac8e392427977f5e316bf3
Abraham <abrahamjuliot@gmail.com>
2020-12-21 05:18:07 +0000

    handle workerScope undefined

--- modules/worker.js
+++ modules/worker.js
@@ -82,17 +82,18 @@ export const getBestWorkerScope = imports => {
 			let type = 'service' // loads fast but is not available in frames
 			let workerScope = await getServiceWorker()
 				.catch(error => console.error(error.message))
-			if (!workerScope.userAgent) {
+			if (!caniuse(() => workerScope.userAgent)) {
+				console.log('trying..')
 				type = 'shared' // no support in Safari, iOS, and Chrome Android
 				workerScope = await getSharedWorker(contentWindow)
 				.catch(error => console.error(error.message))
 			}
-			if (!workerScope.userAgent) {
+			if (!caniuse(() => workerScope.userAgent)) {
 				type = 'dedicated' // simulators & extensions can spoof userAgent
 				workerScope = await getDedicatedWorker(contentWindow)
 				.catch(error => console.error(error.message))
 			}
-			if (workerScope.userAgent) {
+			if (!caniuse(() => workerScope.userAgent)) {
 				const { canvas2d, timezoneHistoryLocation } = workerScope || {}
 				workerScope.system = getOS(workerScope.userAgent)
 				workerScope.device = getUserAgentPlatform({ userAgent: workerScope.userAgent })
