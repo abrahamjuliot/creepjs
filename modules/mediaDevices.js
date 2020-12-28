7a9b5c44c726a3dc5970ff480523112fc6d7ec67
Abraham <abrahamjuliot@gmail.com>
2020-12-28 20:32:33 +0000

    handle media devices error

--- modules/mediaDevices.js
+++ modules/mediaDevices.js
@@ -17,12 +17,20 @@ export const getMediaDevices = imports => {
 				!phantomNavigator.mediaDevices.enumerateDevices) {
 				return resolve()
 			}
-			const mediaDevicesEnumerated = await phantomNavigator.mediaDevices.enumerateDevices()
-			.catch(error => {
-				logTestResult({ test: 'media devices', passed: false })
-				captureError(error)
-				return resolve()
-			})
+			let mediaDevicesEnumerated 
+			try {
+				mediaDevicesEnumerated = await phantomNavigator.mediaDevices.enumerateDevices()
+			}
+			catch (error) {
+				// since Firefox returns a dead object, try in window context
+				mediaDevicesEnumerated = await navigator.mediaDevices.enumerateDevices()
+				.catch(error => {
+					logTestResult({ test: 'media devices', passed: false })
+					captureError(error)
+					return resolve()
+				})
+			}
+			
 			const mediaDevices = (
 				mediaDevicesEnumerated ? 
 				mediaDevicesEnumerated
