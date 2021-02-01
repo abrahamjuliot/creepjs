25fa086d0f3782e7a5b57203a352514c0e680637
Abraham <abrahamjuliot@gmail.com>
2021-02-01 05:28:20 +0000

    tidy

--- creep.js
+++ creep.js
@@ -1471,7 +1471,7 @@ const imports = {
 			)).toFixed(0)
 			const template = `
 				<div class="visitor-info">
-					<div class="ellipsis"><span class="aside-note">script modified 2021-1-3</span></div>
+					<div class="ellipsis"><span class="aside-note">script modified 2021-1-31</span></div>
 					<div class="flex-grid">
 						<div class="col-six">
 							<strong>Browser</strong>
