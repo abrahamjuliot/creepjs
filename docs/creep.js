45862deb33d7c6ebe3d1b3eb79ece79192294a80
Abraham <abrahamjuliot@gmail.com>
2021-02-01 05:22:18 +0000

    mod date

--- docs/creep.js
+++ docs/creep.js
@@ -5905,7 +5905,7 @@
 				)).toFixed(0);
 				const template = `
 				<div class="visitor-info">
-					<div class="ellipsis"><span class="aside-note">script modified 2021-1-3</span></div>
+					<div class="ellipsis"><span class="aside-note">script modified 2021-1-31</span></div>
 					<div class="flex-grid">
 						<div class="col-six">
 							<strong>Browser</strong>
