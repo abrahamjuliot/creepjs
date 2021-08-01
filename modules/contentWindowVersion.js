8942b42f1762ed005352350c0e82fbcb0606cb9e
Abraham <abrahamjuliot@gmail.com>
2021-08-01 16:58:31 +0000

    make Firefox default keys consistent on RFP and inspect

--- modules/contentWindowVersion.js
+++ modules/contentWindowVersion.js
@@ -4,13 +4,34 @@ export const getWindowFeatures = async imports => {
 		require: {
 			captureError,
 			phantomDarkness,
+			isFirefox,
 			logTestResult
 		}
 	} = imports
 
 	try {
 		const start = performance.now()
-		const keys = Object.getOwnPropertyNames(phantomDarkness)
+		let keys = Object.getOwnPropertyNames(phantomDarkness)
+		
+		// if Firefox, remove the 'Event' key and push to end for consistent order
+		// and disregard key known to be missing in RFP mode
+		const firefoxKeyMovedByInspect = 'Event'
+		const varyingKeysMissingInRFP = ['PerformanceNavigationTiming', 'Performance']
+		if (isFirefox) {
+			const index = keys.indexOf(firefoxKeyMovedByInspect)
+			if (index != -1) {
+				keys = keys.slice(0, index).concat(keys.slice(index + 1))
+				keys = [...keys, firefoxKeyMovedByInspect]
+			}
+			varyingKeysMissingInRFP.forEach(key => {
+				const index = keys.indexOf(key)
+				if (index != -1) {
+					keys = keys.slice(0, index).concat(keys.slice(index + 1))
+				}
+				return keys
+			})
+		}
+		
 		const moz = keys.filter(key => (/moz/i).test(key)).length
 		const webkit = keys.filter(key => (/webkit/i).test(key)).length
 		const apple = keys.filter(key => (/apple/i).test(key)).length
