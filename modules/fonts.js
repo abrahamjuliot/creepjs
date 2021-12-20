c7bbaa516c3a6975179715a52dfc6c9c2beb0ce4
Abraham <abrahamjuliot@gmail.com>
2021-12-20 22:23:43 +0000

    revise fonts template and detect desktop apps

--- modules/fonts.js
+++ modules/fonts.js
@@ -11,62 +11,121 @@ const getFontsShortList = () => [
 	'Roboto', // Android, Chrome OS
 ].sort()
 
-const getAppleFonts = () => [
-	'Helvetica Neue'
-]
+const getWindowsFontMap = () => ({
+	// https://docs.microsoft.com/en-us/typography/fonts/windows_11_font_list
+	'7': [
+		'Cambria Math',
+		'Lucida Console'
+	],
+	'8': [
+		'Aldhabi',
+		'Gadugi',
+		'Myanmar Text',
+		'Nirmala UI'
+	],
+	'8.1': [
+		'Leelawadee UI',
+		'Javanese Text',
+		'Segoe UI Emoji'
+	],
+	'10': [
+		'HoloLens MDL2 Assets', // 10 (v1507) +
+		'Segoe MDL2 Assets', // 10 (v1507) +
+		'Bahnschrift', // 10 (v1709) +-
+		'Ink Free', // 10 (v1803) +-
+	],
+	'11': ['Segoe Fluent Icons']
+})
 
-const getWindowsFonts = () => [
-	'Cambria Math',
-	'Lucida Console',
-	'MS Serif',
-	'Segoe UI',
-]
+const getMacOSFontMap = () => ({
+	// Mavericks and below
+	'10.9': [
+		'Helvetica Neue',
+		'Geneva' // mac (not iOS)
+	],
+	// Yosemite
+	'10.10': [
+		'Kohinoor Devanagari Medium',
+		'Luminari'
+	],
+	// El Capitan
+	'10.11': [
+		'PingFang HK Light'
+	],
+	// Sierra: https://support.apple.com/en-ie/HT206872
+	'10.12': [
+		'American Typewriter Semibold',
+		'Futura Bold',
+		'SignPainter-HouseScript Semibold'
+	],
+	// High Sierra: https://support.apple.com/en-me/HT207962
+	// Mojave: https://support.apple.com/en-us/HT208968
+	'10.13-10.14': [
+		'InaiMathi Bold'
+	],
+	// Catalina: https://support.apple.com/en-us/HT210192
+	// Big Sur: https://support.apple.com/en-sg/HT211240
+	'10.15-11': [
+		'Galvji',
+		'MuktaMahee Regular'
+	],
+	// Monterey: https://www.apple.com/my/macos/monterey/features/
+	// https://apple.stackexchange.com/questions/429548/request-for-list-of-fonts-folder-contents-on-monterey
+	//'12': []
+})
+
+const getDesktopAppFontMap = () => ({
+	// docs.microsoft.com/en-us/typography/font-list/ms-outlook
+	'Microsoft Outlook': ['MS Outlook'],
+	// https://community.adobe.com/t5/postscript-discussions/zwadobef-font/m-p/3730427#M785
+	'Adobe Acrobat': ['ZWAdobeF'],
+	// https://wiki.documentfoundation.org/Fonts
+	'LibreOffice': [
+		'Amiri',
+		'DejaVu Sans',
+		'KACSTOffice',
+		'Liberation Mono',
+		'Source Code Pro'
+	]
+})
+
+const getAppleFonts = () => {
+	const macOSFontMap = getMacOSFontMap()
+	return Object.keys(macOSFontMap).map(key => macOSFontMap[key]).flat()
+}
+
+const getWindowsFonts = () => {
+	const windowsFontMap = getWindowsFontMap()
+	return Object.keys(windowsFontMap).map(key => windowsFontMap[key]).flat()
+}
+
+const getDesktopAppFonts = () => {
+	const desktopAppFontMap = getDesktopAppFontMap()
+	return Object.keys(desktopAppFontMap).map(key => desktopAppFontMap[key]).flat()
+}
 
 const getLinuxFonts = () => [
 	'Arimo', // ubuntu, chrome os
-	'Cousine', // ubuntu, chrome os
-	'MONO', // ubuntu, chrome os (not TB)
-	'Jomolhari', // chrome os
-	'Ubuntu', // ubuntu (not TB)
 	'Chilanka', // ubuntu (not TB)
+	'Cousine', // ubuntu, chrome os
+	'Jomolhari', // chrome os
+	'MONO', // ubuntu, chrome os (not TB)
+	'Noto Color Emoji', // Linux
+	'Ubuntu', // ubuntu (not TB)
 ]
 
 const getAndroidFonts = () => [
-	'Dancing Script', // android FF
+	'Dancing Script', // android
+	'Droid Sans Mono', // Android
+	'Roboto' // Android, Chrome OS
 ]
 
-const getGeneralFonts = () => [
-	// Windows
-	'Consolas', //FF and Chrome (not TB)
-	'HELV', // FF (not TB)
-	'Marlett', // chrome
-	// Linux 
-	'Noto Sans JP', // TB linux
-	// Apple
-	'Arial Hebrew', // safari + chrome (not FF or TB)
-	'Arial Rounded MT Bold', // not TB
-	'Geneva', // mac
-	'Apple Chancery', // mac (not TB)
-	'Apple Color Emoji', // ios, chrome, safari (TB, not FF)
-	// Android
-	'Roboto', // android FF, Chrome OS
-	'Droid Sans Mono', // FF android
-	'Cutive Mono', // some android FF
-	// Other
-	'Liberation Mono', // Chrome OS
-	'Noto Sans Yi', // TB on linux and windows, chrome OS, FF android, Mac
-	'Monaco', // android + mac
-	'Palatino', // android + mac + ios
-	'Baskerville', // android + mac
-	'Tahoma' // android, mac, windows (not ios, not chrome os 90)
-]
-
-const getOriginFonts = () => [
+const getFontList = () => [
 	...getAppleFonts(),
 	...getWindowsFonts(),
 	...getLinuxFonts(),
 	...getAndroidFonts(),
-	...getGeneralFonts()
+	...getDesktopAppFonts()
 ].sort()
 
 const originPixelsToInt = pixels => Math.round(2 * pixels.replace('px', ''))
@@ -85,41 +144,41 @@ const getPixelDimensions = style => {
 const getPixelFonts = ({ win, id, chars, baseFonts, families }) => {
 	try {
 		win.document.getElementById(id).innerHTML = `
-		<style>
-			#${id}-detector {
-				--font: '';
-				position: absolute !important;
-				left: -9999px!important;
-				font-size: 256px !important;
-				font-style: normal !important;
-				font-weight: normal !important;
-				letter-spacing: normal !important;
-				line-break: auto !important;
-				line-height: normal !important;
-				text-transform: none !important;
-				text-align: left !important;
-				text-decoration: none !important;
-				text-shadow: none !important;
-				white-space: normal !important;
-				word-break: normal !important;
-				word-spacing: normal !important;
-				/* in order to test scrollWidth, clientWidth, etc. */
-				padding: 0 !important;
-				margin: 0 !important;
-				/* in order to test inlineSize and blockSize */
-				writing-mode: horizontal-tb !important;
-				/* in order to test perspective-origin */
-				/* in order to test origins */
-				transform-origin: unset !important;
-				perspective-origin: unset !important;
-			}
-			#${id}-detector::after {
-				font-family: var(--font);
-				content: '${chars}';
-			}
-		</style>
-		<span id="${id}-detector"></span>
-	`
+			<style>
+				#${id}-detector {
+					--font: '';
+					position: absolute !important;
+					left: -9999px!important;
+					font-size: 256px !important;
+					font-style: normal !important;
+					font-weight: normal !important;
+					letter-spacing: normal !important;
+					line-break: auto !important;
+					line-height: normal !important;
+					text-transform: none !important;
+					text-align: left !important;
+					text-decoration: none !important;
+					text-shadow: none !important;
+					white-space: normal !important;
+					word-break: normal !important;
+					word-spacing: normal !important;
+					/* in order to test scrollWidth, clientWidth, etc. */
+					padding: 0 !important;
+					margin: 0 !important;
+					/* in order to test inlineSize and blockSize */
+					writing-mode: horizontal-tb !important;
+					/* in order to test perspective-origin */
+					/* in order to test origins */
+					transform-origin: unset !important;
+					perspective-origin: unset !important;
+				}
+				#${id}-detector::after {
+					font-family: var(--font);
+					content: '${chars}';
+				}
+			</style>
+			<span id="${id}-detector"></span>
+		`
 		const span = win.document.getElementById(`${id}-detector`)
 		const detectedViaTransform = new Set()
 		const detectedViaPerspective = new Set()
@@ -159,9 +218,9 @@ const getPixelFonts = ({ win, id, chars, baseFonts, families }) => {
 	}
 }
 
-const getFontFaceLoadFonts = async list => {
+const getFontFaceLoadFonts = async fontList => {
 	try {
-		const fontFaceList = list.map(font => new FontFace(font, `local("${font}")`))
+		const fontFaceList = fontList.map(font => new FontFace(font, `local("${font}")`))
 		const responseCollection = await Promise
 			.allSettled(fontFaceList.map(font => font.load()))
 		const fonts = responseCollection.reduce((acc, font) => {
@@ -177,88 +236,15 @@ const getFontFaceLoadFonts = async list => {
 	}
 }
 
-const getPlatformVersion = async () => {
-	const windowsFonts = {
-		// https://docs.microsoft.com/en-us/typography/fonts/windows_11_font_list
-		'7': [
-			'Cambria Math',
-			'Lucida Console'
-		],
-		'8': [
-			'Aldhabi',
-			'Gadugi',
-			'Myanmar Text',
-			'Nirmala UI'
-		],
-		'8.1': [
-			'Leelawadee UI',
-			'Javanese Text',
-			'Segoe UI Emoji'
-		],
-		'10': [
-			'HoloLens MDL2 Assets', // 10 (v1507) +
-			'Segoe MDL2 Assets', // 10 (v1507) +
-			'Bahnschrift', // 10 (v1709) +-
-			'Ink Free', // 10 (v1803) +-
-		],
-		'11': ['Segoe Fluent Icons']
-	}
-
-	const macOSFonts = {
-		// Mavericks and below
-		'10.9': [
-			'Helvetica Neue',
-			'Geneva'
-		],
-		// Yosemite
-		'10.10': [
-			'Kohinoor Devanagari Medium',
-			'Luminari'
-		],
-		// El Capitan
-		'10.11': [
-			'PingFang HK Light'
-		],
-		// Sierra: https://support.apple.com/en-ie/HT206872
-		'10.12': [
-			'American Typewriter Semibold',
-			'Futura Bold',
-			'SignPainter-HouseScript Semibold'
-		],
-		// High Sierra: https://support.apple.com/en-me/HT207962
-		// Mojave: https://support.apple.com/en-us/HT208968
-		'10.13-10.14': [
-			'InaiMathi Bold'
-		],
-		// Catalina: https://support.apple.com/en-us/HT210192
-		// Big Sur: https://support.apple.com/en-sg/HT211240
-		'10.15-11': [
-			'Galvji',
-			'MuktaMahee Regular'
-		],
-		// Monterey: https://www.apple.com/my/macos/monterey/features/
-		// https://apple.stackexchange.com/questions/429548/request-for-list-of-fonts-folder-contents-on-monterey
-		//'12': []
-	}
-
-	const fontList = [
-		...Object.keys(windowsFonts).map(key => windowsFonts[key]).flat(),
-		...Object.keys(macOSFonts).map(key => macOSFonts[key]).flat()
-	]
-	const fontFaceList = fontList.map(font => new FontFace(font, `local("${font}")`))
-	const responseCollection = await Promise.allSettled(fontFaceList.map(font => font.load()))
-	const fonts = responseCollection.reduce((acc, font) => {
-		return font.status == 'fulfilled' ? [...acc, font.value.family] : acc
-	}, [])
-
-	const getWindows = fonts => {
+const getPlatformVersion = fonts => {
+	const getWindows = ({ fonts, fontMap }) => {
 		const fontVersion = {
-			['11']: windowsFonts['11'].find(x => fonts.includes(x)),
-			['10']: windowsFonts['10'].find(x => fonts.includes(x)),
-			['8.1']: windowsFonts['8.1'].find(x => fonts.includes(x)),
-			['8']: windowsFonts['8'].find(x => fonts.includes(x)),
+			['11']: fontMap['11'].find(x => fonts.includes(x)),
+			['10']: fontMap['10'].find(x => fonts.includes(x)),
+			['8.1']: fontMap['8.1'].find(x => fonts.includes(x)),
+			['8']: fontMap['8'].find(x => fonts.includes(x)),
 			// require complete set of Windows 7 fonts
-			['7']: windowsFonts['7'].filter(x => fonts.includes(x)).length == windowsFonts['7'].length
+			['7']: fontMap['7'].filter(x => fonts.includes(x)).length == fontMap['7'].length
 		}
 		const hash = (
 			'' + Object.keys(fontVersion).sort().filter(key => !!fontVersion[key])
@@ -280,15 +266,15 @@ const getPlatformVersion = async () => {
 		return version ? `Windows ${version}` : undefined
 	}
 
-	const getMacOS = fonts => {
+	const getMacOS = ({ fonts, fontMap }) => {
 		const fontVersion = {
-			['10.15-11']: macOSFonts['10.15-11'].find(x => fonts.includes(x)),
-			['10.13-10.14']: macOSFonts['10.13-10.14'].find(x => fonts.includes(x)),
-			['10.12']: macOSFonts['10.12'].find(x => fonts.includes(x)),
-			['10.11']: macOSFonts['10.11'].find(x => fonts.includes(x)),
-			['10.10']: macOSFonts['10.10'].find(x => fonts.includes(x)),
+			['10.15-11']: fontMap['10.15-11'].find(x => fonts.includes(x)),
+			['10.13-10.14']: fontMap['10.13-10.14'].find(x => fonts.includes(x)),
+			['10.12']: fontMap['10.12'].find(x => fonts.includes(x)),
+			['10.11']: fontMap['10.11'].find(x => fonts.includes(x)),
+			['10.10']: fontMap['10.10'].find(x => fonts.includes(x)),
 			// require complete set of 10.9 fonts
-			['10.9']: macOSFonts['10.9'].filter(x => fonts.includes(x)).length == macOSFonts['10.9'].length
+			['10.9']: fontMap['10.9'].filter(x => fonts.includes(x)).length == fontMap['10.9'].length
 		}
 		const hash = (
 			'' + Object.keys(fontVersion).sort().filter(key => !!fontVersion[key])
@@ -305,12 +291,21 @@ const getPlatformVersion = async () => {
 		return version ? `macOS ${version}` : undefined
 	}
 
-	return {
-		fonts,
-		version: getWindows(fonts) || getMacOS(fonts)
-	}
+	return  (
+		getWindows({ fonts, fontMap: getWindowsFontMap() }) ||
+		getMacOS({ fonts, fontMap: getMacOSFontMap() })
+	)
 }
 
+const getDesktopApps = fonts => {
+	const desktopAppFontMap = getDesktopAppFontMap()
+	const apps = Object.keys(desktopAppFontMap).reduce((acc, key) => {
+		const appFontSet = desktopAppFontMap[key]
+		const match = appFontSet.filter(x => fonts.includes(x)).length == appFontSet.length
+		return match ? [...acc, key] : acc
+	}, [])
+	return apps
+}
 
 export const getFonts = async imports => {
 
@@ -326,20 +321,18 @@ export const getFonts = async imports => {
 	try {
 		await new Promise(setTimeout).catch(e => { })
 		const start = performance.now()
-		const win = phantomDarkness ? phantomDarkness : window
+		const win = phantomDarkness || window
 		const doc = win.document
-
 		const id = `font-fingerprint`
 		const div = doc.createElement('div')
 		div.setAttribute('id', id)
 		doc.body.appendChild(div)
-		const originFontsList = getOriginFonts()
 		const baseFonts = ['monospace', 'sans-serif', 'serif']
-		const families = originFontsList.reduce((acc, font) => {
+		const fontShortList = getFontsShortList()
+		const families = fontShortList.reduce((acc, font) => {
 			baseFonts.forEach(baseFont => acc.push(`'${font}', ${baseFont}`))
 			return acc
 		}, [])
-
 		const pixelFonts = getPixelFonts({
 			win,
 			id,
@@ -347,36 +340,29 @@ export const getFonts = async imports => {
 			baseFonts,
 			families
 		})
-
 		const compressToList = fontObject => Object.keys(fontObject).reduce((acc, key) => {
 			return [...acc, ...fontObject[key]]
 		}, [])
-
-		const [
-			fontFaceLoadFonts,
-			platformVersion
-		] = await Promise.all([
-			getFontFaceLoadFonts(getFontsShortList()),
-			getPlatformVersion()
-		])
-		//console.log(platformVersion.version)
-		//console.log(platformVersion.fonts.join('\n'))
 		const originFonts = [...new Set(compressToList(pixelFonts))]
+
+		const fontList = getFontList()
+		const fontFaceLoadFonts = await getFontFaceLoadFonts(fontList)
+		const platformVersion = getPlatformVersion(fontFaceLoadFonts)
+		const apps = getDesktopApps(fontFaceLoadFonts)
 
 		logTestResult({ start, test: 'fonts', passed: true })
 		return {
 			fontFaceLoadFonts,
 			pixelFonts,
 			originFonts,
-			platformVersion: (platformVersion||{}).version,
-			platformFonts: (platformVersion||{}).fonts
+			platformVersion,
+			apps
 		}
 	} catch (error) {
 		logTestResult({ test: 'fonts', passed: false })
 		captureError(error)
 		return
 	}
-
 }
 
 export const fontsHTML = ({ fp, note, modal, count, hashSlice, hashMini }) => {
@@ -384,7 +370,6 @@ export const fontsHTML = ({ fp, note, modal, count, hashSlice, hashMini }) => {
 		return `
 		<div class="col-six undefined">
 			<strong>Fonts</strong>
-			<div>version: ${note.blocked}</div>
 			<div>origin (0): ${note.blocked}</div>
 			<div>load (0):</div>
 			<div class="block-text">${note.blocked}</div>
@@ -395,40 +380,11 @@ export const fontsHTML = ({ fp, note, modal, count, hashSlice, hashMini }) => {
 			$hash,
 			fontFaceLoadFonts,
 			originFonts,
-			platformVersion
+			platformVersion,
+			apps
 		}
 	} = fp
 
-	const apple = new Set(getAppleFonts())
-	const linux = new Set(getLinuxFonts())
-	const windows = new Set(getWindowsFonts())
-	const android = new Set(getAndroidFonts())
-
-	const systemClass = [...originFonts.reduce((acc, font) => {
-		if (!acc.has('Apple') && apple.has(font)) {
-			acc.add('Apple')
-			return acc
-		}
-		if (!acc.has('Linux') && linux.has(font)) {
-			acc.add('Linux')
-			return acc
-		}
-		if (!acc.has('Windows') && windows.has(font)) {
-			acc.add('Windows')
-			return acc
-		}
-		if (!acc.has('Android') && android.has(font)) {
-			acc.add('Android')
-			return acc
-		}
-		return acc
-	}, new Set())]
-	const chromeOnAndroid = (
-		'' + ((originFonts || []).sort()) == 'Baskerville,Monaco,Palatino,Tahoma'
-	)
-	if (!systemClass.length && chromeOnAndroid) {
-		systemClass.push('Android')
-	}
 	const icon = {
 		'Linux': '<span class="icon linux"></span>',
 		'Apple': '<span class="icon apple"></span>',
@@ -436,8 +392,6 @@ export const fontsHTML = ({ fp, note, modal, count, hashSlice, hashMini }) => {
 		'Android': '<span class="icon android"></span>',
 		'CrOS': '<span class="icon cros"></span>'
 	}
-	const systemClassIcons = systemClass.map(name => icon[name])
-	const originHash = hashMini(originFonts)
 
 	const systemMap = {
 		'Lucida Console': [icon.Windows, 'Windows'],
@@ -446,32 +400,40 @@ export const fontsHTML = ({ fp, note, modal, count, hashSlice, hashMini }) => {
 		'Noto Color Emoji,Ubuntu': [icon.Linux, 'Linux Ubuntu'],
 		'Noto Color Emoji,Roboto': [icon.CrOS, 'Chrome OS'],
 		'Droid Sans Mono,Roboto': [icon.Android, 'Android'],
-		'Droid Sans Mono,Noto Color Emoji,Roboto': [`${icon.Linux}${icon.Android}`, 'Linux Android'],
 		'Helvetica Neue': [icon.Apple, 'iOS'],
 		'Geneva,Helvetica Neue': [icon.Apple, 'Mac']
 	}
 
-	const fontFaceLoadFontsString = '' + (fontFaceLoadFonts.sort())
-	const system = systemMap[fontFaceLoadFontsString]
+	const originFontString = ''+(originFonts.sort())
+	const fontFaceLoadHash = hashMini(fontFaceLoadFonts)
+	const system = systemMap[originFontString]
 
 	return `
 	<div class="col-six">
 		<strong>Fonts</strong><span class="hash">${hashSlice($hash)}</span>
-		<div>version: ${platformVersion || note.unknown}</div>
-		<div class="help" title="CSSStyleDeclaration.setProperty()\ntransform-origin\nperspective-origin">origin (${originFonts ? count(originFonts) : '0'}/${'' + getOriginFonts().length}): ${
-		originFonts.length ? modal(
-			'creep-fonts', originFonts.map(font => `<span style="font-family:'${font}'">${font}</span>`).join('<br>'),
-			`${systemClass.length ? `${systemClassIcons.join('')}${originHash}` : originHash}`
-		) : note.unknown
-		}</div>
-		<div class="help" title="FontFace.load()">load (${fontFaceLoadFonts ? count(fontFaceLoadFonts) : '0'}/${'' + getFontsShortList().length}): ${
-		system ? system[1] : ''
+		<div class="help" title="CSSStyleDeclaration.setProperty()\ntransform-origin\nperspective-origin">origin (${originFonts ? count(originFonts) : '0'}/${'' + getFontsShortList().length}): ${originFontString ? `${system ? system[0] : ''}${originFontString}`: note.unknown }</div>
+		<div class="help" title="FontFace.load()">load (${fontFaceLoadFonts ? count(fontFaceLoadFonts) : '0'}/${'' + getFontList().length}): ${
+			!fontFaceLoadFonts.length ? note.unknown : modal(
+				'creep-fonts',
+				fontFaceLoadFonts.map(font => `<span style="font-family:'${font}'">${font}</span>`).join('<br>'),
+				fontFaceLoadHash
+			)
 		}</div>
 		<div class="block-text">
-			<div>${
-		fontFaceLoadFonts.length ? `${system ? system[0] : ''}${fontFaceLoadFontsString}` :
-			note.unknown
-		}</div>
+			<div>
+				${platformVersion ? `platform version: ${platformVersion}<br>` : ((fonts) => {
+					return (
+						((''+fonts).match(/Lucida Console/)||[]).length ? `${icon.Windows}Lucida Console...` :
+						((''+fonts).match(/Droid Sans Mono|Roboto/g)||[]).length == 2 ? `${icon.Android}Droid Sans Mono,Roboto...` :
+						((''+fonts).match(/Noto Color Emoji|Roboto/g)||[]).length == 2 ? `${icon.CrOS}Noto Color Emoji,Roboto...` :
+						((''+fonts).match(/Noto Color Emoji/)||[]).length ? `${icon.Linux}Noto Color Emoji...` :
+						((''+fonts).match(/Arimo/)||[]).length ? `${icon.Linux}Arimo...` :
+						((''+fonts).match(/Helvetica Neue/g)||[]).length == 2 ? `${icon.Apple}Helvetica Neue...` :
+						`${fonts[0]}...`
+					)
+				})(fontFaceLoadFonts)}
+				${''+apps ? `apps: ${apps.join(', ')}` : ''}
+			</div>
 		</div>
 	</div>
 	`
