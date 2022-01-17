c4fb97f5ae9a697643fd166e85a68363a64985a1
Abraham <abrahamjuliot@gmail.com>
2022-01-17 05:04:26 +0000

    revert stun servers to one

--- modules/webrtc.js
+++ modules/webrtc.js
@@ -138,7 +138,7 @@ export const getWebRTCData = async imports => {
 					{
 						urls: [
 							'stun:stun4.l.google.com:19302?transport=udp',
-							'stun:stun3.l.google.com:19302?transport=udp',
+							//'stun:stun3.l.google.com:19302?transport=udp',
 							//'stun:stun2.l.google.com:19302?transport=udp',
 							//'stun:stun1.l.google.com:19302?transport=udp',
 							//'stun:stun.l.google.com:19302?transport=udp',
