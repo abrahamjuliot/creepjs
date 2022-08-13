0677e35df570385226c2914a700f9447ee69ed39
Abraham <abrahamjuliot@gmail.com>
2022-08-13 04:54:35 +0000

    chore: force renew

--- src/creep.ts
+++ src/creep.ts
@@ -579,7 +579,7 @@ import getBestWorkerScope, { Scope, spawnWorker, workerScopeHTML } from './worke
 		capturedErrors: !!errorsLen,
 		lies: !!liesLen,
 		resistance: fp.resistance || undefined,
-		forceRenew: 1660096196611,
+		forceRenew: 1660365177291,
 	}
 
 	console.log('%c✔ stable fingerprint passed', 'color:#4cca9f')
