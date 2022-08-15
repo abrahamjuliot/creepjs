82772139f5edb78ad3c773c55314c997bcc99dce
Abraham <abrahamjuliot@gmail.com>
2022-08-15 05:12:06 +0000

    fix: remove desynchronized

--- src/headless/getPlatformEstimate.ts
+++ src/headless/getPlatformEstimate.ts
@@ -4,17 +4,12 @@ import { Platform } from './constants'
 export default function getPlatformEstimate(): [Record<string, number>, number] | null {
     if (!IS_BLINK) return null
     const v80 = 'getVideoPlaybackQuality' in HTMLVideoElement.prototype
-    const v81 = CSS.supports('color-scheme: initial')
     const v84 = CSS.supports('appearance: initial')
     const v86 = 'DisplayNames' in Intl
     const v88 = CSS.supports('aspect-ratio: initial')
     const v89 = CSS.supports('border-end-end-radius: initial')
     const v95 = 'randomUUID' in Crypto.prototype
     const hasBarcodeDetector = 'BarcodeDetector' in window
-    const canvas = document.createElement('canvas')
-    const hasDesynchronized = (
-        typeof canvas?.getContext('2d')?.getContextAttributes?.().desynchronized == 'boolean'
-    )
     const hasDownlinkMax = 'downlinkMax' in (window.NetworkInformation?.prototype || {})
     const hasContentIndex = 'ContentIndex' in window
     const hasContactsManager = 'ContactsManager' in window
@@ -33,7 +28,6 @@ export default function getPlatformEstimate(): [Record<string, number>, number] 
             ...hasFeature(v88, hasBarcodeDetector),
             ...hasFeature(v84, hasContentIndex),
             ...hasFeature(v80, hasContactsManager),
-            ...hasFeature(v81, hasDesynchronized),
             hasDownlinkMax,
             ...hasFeature(v95, !hasEyeDropper),
             ...hasFeature(v86, !hasFileSystemWritableFileStream),
@@ -46,7 +40,6 @@ export default function getPlatformEstimate(): [Record<string, number>, number] 
             ...hasFeature(v88, hasBarcodeDetector),
             ...hasFeature(v84, !hasContentIndex),
             ...hasFeature(v80, !hasContactsManager),
-            ...hasFeature(v81, hasDesynchronized),
             hasDownlinkMax,
             ...hasFeature(v95, hasEyeDropper),
             ...hasFeature(v86, hasFileSystemWritableFileStream),
@@ -59,7 +52,6 @@ export default function getPlatformEstimate(): [Record<string, number>, number] 
             ...hasFeature(v88, !hasBarcodeDetector),
             ...hasFeature(v84, !hasContentIndex),
             ...hasFeature(v80, !hasContactsManager),
-            ...hasFeature(v81, hasDesynchronized),
             !hasDownlinkMax,
             ...hasFeature(v95, hasEyeDropper),
             ...hasFeature(v86, hasFileSystemWritableFileStream),
@@ -72,7 +64,6 @@ export default function getPlatformEstimate(): [Record<string, number>, number] 
             ...hasFeature(v88, hasBarcodeDetector),
             ...hasFeature(v84, !hasContentIndex),
             ...hasFeature(v80, !hasContactsManager),
-            ...hasFeature(v81, !hasDesynchronized),
             !hasDownlinkMax,
             ...hasFeature(v95, hasEyeDropper),
             ...hasFeature(v86, hasFileSystemWritableFileStream),
@@ -85,7 +76,6 @@ export default function getPlatformEstimate(): [Record<string, number>, number] 
             ...hasFeature(v88, !hasBarcodeDetector),
             ...hasFeature(v84, !hasContentIndex),
             ...hasFeature(v80, !hasContactsManager),
-            ...hasFeature(v81, hasDesynchronized),
             !hasDownlinkMax,
             ...hasFeature(v95, hasEyeDropper),
             ...hasFeature(v86, hasFileSystemWritableFileStream),
