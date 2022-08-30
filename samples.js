e492f56265d9dd7c67f299d54de0d1bdcbdf9a5e
Abraham <abrahamjuliot@gmail.com>
2022-08-30 06:06:34 +0000

    fix: use axios

--- samples.js
+++ samples.js
@@ -1,16 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs')
-const fetch = require('node-fetch')
+const axios = require('axios')
 
 // This only needs to be manually updated weekly
 const URL = 'https://script.google.com/macros/s/AKfycbw26MLaK1PwIGzUiStwweOeVfl-sEmIxFIs5Ax7LMoP1Cuw-s0llN-aJYS7F8vxQuVG-A/exec'
 const FILE_PATH = 'docs/data/samples.json'
 
 // @ts-expect-error if not supported
-fetch(URL)
-  .then((res) => res.json())
-  .then((data) => {
-    fs.writeFile(FILE_PATH, JSON.stringify(data), (err) => {
+axios.get(URL)
+  .then((res) => {
+    fs.writeFile(FILE_PATH, JSON.stringify(res.data), (err) => {
       if (err) throw err
       console.log('samples updated!')
     })
