973ef3de5997e3f3c6455e1021ea5fbbd8051bf2
ginlink <ginlink1998@gmail.com>
2023-07-10 08:52:41 +0800

    fix: hasLied&hasErrors are undefined (#224)
    * fix: hasLied&hasErrors are undefined
    
    * fix: hasLied&hasErrors

--- src/creep.ts
+++ src/creep.ts
@@ -672,7 +672,8 @@ import getBestWorkerScope, { Scope, spawnWorker, workerScopeHTML } from './worke
 
 	// patch dom
 	const hasTrash = !!trashLen
-	const { lies: hasLied, capturedErrors: hasErrors } = creep
+	const hasLied = !!fp.lies?.totalLies
+	const hasErrors = !!fp.capturedErrors?.data?.length
 
 	const el = document.getElementById('fingerprint-data')
 	patch(el, html`
